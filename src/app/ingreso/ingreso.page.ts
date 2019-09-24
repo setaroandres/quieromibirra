3af56b76d2cbc2a49eb6ba3c6f57fbbb837ca273
ruge23 <ruizgerezr@gmail.com>
2019-09-24 16:01:03 -0300

    ingreso

--- src/app/ingreso/ingreso.page.ts
+++ src/app/ingreso/ingreso.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-ingreso',
   templateUrl: './ingreso.page.html',
   styleUrls: ['./ingreso.page.scss'],
 })
 export class IngresoPage implements OnInit {
+  slideOneForm: FormGroup;
+  public submitAttempt: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public formBuilder: FormBuilder
+  ) {
+    this.slideOneForm = formBuilder.group({
+      firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  save() {
+
+    this.submitAttempt = true;
+
+    if (!this.slideOneForm.valid) {
+      console.log("nooo!")
+      console.log(this.slideOneForm.value);
+    } else {
+      console.log("success!")
+      this.submitAttempt = false;
+      console.log(this.slideOneForm.value);
+      this.router.navigateByUrl('/tutorial');
+    }
+
   }
 
 }
